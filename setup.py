447be4457cbca61ad5307b8270e531df0ee8db1b
Arkan Aulia Farhan <32859227+arkanaulia@users.noreply.github.com>
2023-01-25 09:57:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     py_modules=["auto_subtitle"],
     author="Miguel Piedrafita",
     install_requires=[
-        'youtube-dl',
-        'whisper @ git+https://github.com/openai/whisper.git@main#egg=whisper'
+        
     ],
     description="Automatically generate and embed subtitles into your videos",
     entry_points={
